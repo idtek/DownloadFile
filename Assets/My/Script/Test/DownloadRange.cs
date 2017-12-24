8d72cbdd65b20bc47e648468eed84c7d94a23806
zzy <1156846412@qq.com>
2017-12-24 23:50:17 +0800

    重新实现了断点续传下载类

--- Assets/My/Script/Test/DownloadRange.cs
+++ Assets/My/Script/Test/DownloadRange.cs
@@ -3,85 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.IO;
+using System;
 
 namespace DownloadFileNW
 {
     public class DownloadRange : DownloadHandlerScript
     {
         private string Path;
-        private string ConfigPath;
-        private bool IsFirst = true;
-        private ulong DataSize = 0;
-        private ulong lastIndex = 0;
-        private int count = 0;
-        private FileStream stream;
-        private bool IsCompleted = false;
-
-        public ulong LastIndex
-        {
-            get
-            {
-                return lastIndex;
-            }
-            private set
-            {
-                lastIndex = value;
-            }
-        }
+        private FileStream FileStream;
+        private UnityWebRequest UnityWebRequest;
+        private long LocalFileSize = 0;
+        private long TotalFileSize = 0;
+        private long CurFileSize = 0;
 
         public DownloadRange(string path,UnityWebRequest request)
         {
             Path = path;
-            ConfigPath = Path + ".config";
-            if (FileTools.FileExists(ConfigPath))
+            FileStream = new FileStream(Path, FileMode.Append, FileAccess.Write);
+            if (FileTools.FileExists(Path))
             {
-                LastIndex = ulong.Parse(FileTools.ReadFileUTf8(ConfigPath));
+                LocalFileSize = new FileInfo(Path).Length;
             }
-            stream = new FileStream(path, FileMode.Append, FileAccess.Write);
+            UnityWebRequest = request;
+            UnityWebRequest.SetRequestHeader("Range", "bytes=" + LocalFileSize + "-");
         }
 
-        public void DownloadRange_completed(AsyncOperation obj)
+        new public void Dispose()
         {
-            Debug.LogError("结束了");
-            LastIndex += DataSize;
-            if (IsCompleted)
+            base.Dispose();
+            Debug.Log("被中断了");
+            if (FileStream != null)
             {
-                if (FileTools.FileExists(ConfigPath))
-                {
-                    FileTools.DeleteFile(ConfigPath);
-                }
+                FileStream.Close();
+                FileStream = null;
             }
-            else
-            {
-                FileTools.WriteFileUtf8Create(ConfigPath, LastIndex.ToString());
-            }
-            Debug.Log(DataSize);
-            stream.Close();
         }
 
         protected override void CompleteContent()
         {
-            IsCompleted = true;
+            if (FileStream != null)
+            {
+                FileStream.Close();
+                FileStream = null;
+            }
+        }
+
+        protected override byte[] GetData()
+        {
+            return null;
         }
 
         protected override float GetProgress()
         {
-            return base.GetProgress();
+            return TotalFileSize == 0 ? 0 : ((float)(CurFileSize + LocalFileSize)) / (TotalFileSize + LocalFileSize);
+        }
+
+        protected override string GetText()
+        {
+            return null;
         }
 
         protected override void ReceiveContentLength(int contentLength)
         {
-            base.ReceiveContentLength(contentLength);
+            TotalFileSize = contentLength;
         }
 
         protected override bool ReceiveData(byte[] data, int dataLength)
         {
-            DataSize = DataSize + (ulong)dataLength;
-
-            Debug.Log(++count);
-            stream.Write(data, 0, dataLength);
-            Debug.Log(dataLength);
-            Debug.Log(count+"\n---------------------------");
+            if (data == null || dataLength < 1)
+            {
+                return false;
+            }
+            FileStream.Write(data, 0, dataLength);
+            FileStream.Flush();
+            CurFileSize += dataLength;
             return true;
         }
     }
--- Assets/My/Script/Test/DownloadRangeTest.cs
+++ Assets/My/Script/Test/DownloadRangeTest.cs
@@ -9,46 +9,21 @@ public class DownloadRangeTest : MonoBehaviour {
     UnityWebRequest request;
     private void Awake()
     {
-        //string url = "http://d1.music.126.net/dmusic/netease-cloud-music_1.1.0_amd64_deepin.deb";
-        //request = new UnityWebRequest(url);
-        //request.downloadHandler = new DownloadHandlerBuffer();
-        //request.SetRequestHeader("Range", "bytes=0-500");
-        //request.SendWebRequest().completed += DownloadRangeTest_completed; 
         test();
-        //test2();
-        
     }
 
-    void test2()
+    void test()
     {
         string url = "https://d1.music.126.net/dmusic/cloudmusicsetup_2_2_3_195673.exe";
         request = new UnityWebRequest(url);
-        request.downloadHandler = new DownloadHandlerBuffer();
+        DownloadRange download = new DownloadRange("D:/网易云.exe",request);
+        request.downloadHandler = download;
         request.SendWebRequest().completed += DownloadRangeTest_completed1;
     }
 
     private void DownloadRangeTest_completed1(AsyncOperation obj)
     {
-        FileTools.WriteFileBytesAppend("D:/网易云.exe", request.downloadHandler.data);
-    }
-
-    private void test()
-    {
-        string url = "https://d1.music.126.net/dmusic/cloudmusicsetup_2_2_3_195673.exe";
-        request = new UnityWebRequest(url);
-        DownloadRange downloadRange = new DownloadRange("D:/网易云音乐.exe",request);
-        request.downloadHandler = downloadRange;
-        string temp = "bytes=" + downloadRange.LastIndex.ToString() + "-";
-        Debug.Log(temp);
-        request.SetRequestHeader("Range", temp);
-        UnityWebRequestAsyncOperation unityWebRequestAsyncOperation = request.SendWebRequest();
-        unityWebRequestAsyncOperation.completed += downloadRange.DownloadRange_completed;
-        unityWebRequestAsyncOperation.completed += UnityWebRequestAsyncOperation_completed;
-    }
-
-    private void UnityWebRequestAsyncOperation_completed(AsyncOperation obj)
-    {
-        Debug.Log(request.GetResponseHeader("Content-Range"));
+        Debug.Log("下载结束");
     }
 
     private void Start()
@@ -57,6 +32,10 @@ public class DownloadRangeTest : MonoBehaviour {
 
     private void Update()
     {
+        if (!request.isDone)
+        {
+            Debug.Log(request.downloadProgress);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             request.Abort();
